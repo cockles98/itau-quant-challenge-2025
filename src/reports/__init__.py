e11f301cc1ad2f395f102584f343680c5ed9430d
Felipe Cockles <felipe.cockles@hotmail.com>
2025-09-25 15:43:02 -0300

    cleaning repo

--- src/reports/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Reporting package exports."""
-
-from .reporting import plot_equity_curves, table_kpis
-from .build_pdf import build_pdf
-
-__all__ = ["plot_equity_curves", "table_kpis", "build_pdf"]
--- src/risk/__init__.py
+++
@@ -1,5 +0,0 @@
-﻿"""Risk management entry points."""
-
-from .position_sizing import atr, atr_risk_normalize, scale_to_vol
-
-__all__ = ["atr", "atr_risk_normalize", "scale_to_vol"]
